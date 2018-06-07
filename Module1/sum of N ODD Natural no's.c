7b1a109931fa5483887a7781f14116e910737aa0
Ramesh_G <37336675+rameshnickms@users.noreply.github.com>
2018-06-07 19:37:56 +0530

    Create sum of N ODD Natural no's.c

---
+++ Module1/sum of N ODD Natural no's.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,j,k;
+    int f=0;
+    scanf("%d",&n);
+    int a[n],sum=0;
+    for (int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    while(f<n)
+    {
+    for(int j=0; j<a[f];j++)
+    {
+        sum+= ((2*j)+1);
+    }
+        printf("%d\n",sum);
+    f++;
+    sum=0;
+    }   
+    return 0;
+}
